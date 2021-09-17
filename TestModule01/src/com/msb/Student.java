be47cf8f80977e3696021926229c6d23c1b43d10
wanganlong <chinawal@126.com>
2021-09-17 12:31:13 +0800

    第一个进行数据的提交...

---
+++ TestModule01/src/com/msb/Student.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther: wanganlong
+ * @Date:2021/9/17 - 09 -17 - 12:27
+ * @Description：
+ * @version:1.0
+ */
+public class Student {
+    private int age;
+    private String name;
+}
